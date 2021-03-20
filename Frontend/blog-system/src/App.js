257941c46010ae38ebc78edab536f4a9577e1429
Ivan Nikolov <nikolov.iv@gmail.com>
2021-03-20 16:32:07 +0200

    Add basic Home page and footer

--- Frontend/blog-system/src/App.js
+++ Frontend/blog-system/src/App.js
@@ -1,5 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
+import Home from "./components/Pages/Home";
 import "./App.css";
 
 function App() {
@@ -7,8 +9,9 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
-        <Route />
+        <Route path="/" component={Home} />
       </Switch>
+      <Footer/>
     </div>
   );
 }
---
+++ Frontend/blog-system/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+const Footer = () => {
+    return(
+        <h4>All rights reserved!</h4>
+    );
+}
+
+export default Footer;
