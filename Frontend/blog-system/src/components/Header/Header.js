ce4d91ceddd47ab3224bfec0529ef95c5dd5a411
Ivan Nikolov <nikolov.iv@gmail.com>
2021-03-20 16:12:01 +0200

    Add react-router-dom, basic navbar

---
+++ Frontend/blog-system/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import { React } from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Register</li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
