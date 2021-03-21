47667c35fc31f0f0f672b9b893a55c53cc30a695
Ivan Nikolov <nikolov.iv@gmail.com>
2021-03-21 10:43:11 +0200

    Add BlogSystemDbContext

---
+++ Backend/BlogSystem/BlogSystem.Data/BlogSystemDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    public class BlogSystemDbContext : DbContext
+    {
+        public BlogSystemDbContext(DbContextOptions<BlogSystemDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
